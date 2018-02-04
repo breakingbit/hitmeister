06dc4ad2639f6b910026904a2760b350a4d09afa
michael-herwig <michael.herwig@hotmail.de>
2018-02-04 15:58:34 +0100

    Removed CodeSmell on EANType

--- Source/BreakingBit.Hitmeister.API/Models/EANType.cs
+++ Source/BreakingBit.Hitmeister.API/Models/EANType.cs
@@ -3,7 +3,9 @@
     /// <summary>
     /// The underlying norm an <see cref="EAN"/> conforms to
     /// </summary>
+#pragma warning disable S2342 // EAN is an abbreviation
     public enum EANType : uint
+#pragma warning restore S2342 // EAN is an abbreviation
     {
         /// <summary>
         /// A 13 digits long article number

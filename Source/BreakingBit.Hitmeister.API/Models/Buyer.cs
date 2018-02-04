34149dea9cc3d80ac3419b84b6eaeca0484511c8
michael-herwig <michael.herwig@hotmail.de>
2018-02-04 22:24:46 +0100

    Added buyer model

---
+++ Source/BreakingBit.Hitmeister.API/Models/Buyer.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace BreakingBit.Hitmeister.API.Models
+{
+    /// <summary>
+    /// The particulars of a buyer
+    /// </summary>
+    public sealed class Buyer
+    {
+        /// <summary>
+        /// The buyer's internal id, unique across all buyers
+        /// </summary>
+        /// <value>Unique identifier</value>
+        [JsonProperty("id_buyer", Required = Required.Always)]
+        public int Identifier { get; set; }
+
+        /// <summary>
+        /// The email address given by the user
+        /// </summary>
+        /// <value>An email address</value>
+        [JsonProperty("email", Required = Required.Always)]
+        public string Email { get; set; }
+    }
+}

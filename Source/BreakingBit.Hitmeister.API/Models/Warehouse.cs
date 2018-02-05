75a14ec36ce36a017d844042e94a9faffe139051
michael-herwig <michael.herwig@hotmail.de>
2018-02-05 21:42:08 +0100

    Added Warehouse models

---
+++ Source/BreakingBit.Hitmeister.API/Models/Warehouse.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+
+namespace BreakingBit.Hitmeister.API.Models
+{
+    /// <summary>
+    /// Data of a building used to store products.
+    /// </summary>
+    public sealed class Warehouse
+    {
+        /// <summary>
+        /// Internal identifier of Warehouse, unique across all Warehouses
+        /// </summary>
+        /// <value>Unique identifier</value>
+        [JsonProperty("id_warehouse", Required = Required.Always)]
+        public long Identifier { get; set; }
+
+        /// <summary>
+        /// A name given by the user for identification.
+        /// </summary>
+        /// <value>User defined string</value>
+        [JsonProperty("name", Required = Required.Always)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <value></value>
+        [JsonProperty("address", Required = Required.Always)]
+        public WarehouseAddress Address { get; set; }
+
+        /// <summary>
+        /// Is pickup possible for this Warehouse.
+        /// </summary>
+        /// <value>Flag</value>
+        [JsonProperty("is_pickup_possible", Required = Required.Always)]
+        public bool IsPickupPossible { get; set; }
+
+        /// <summary>
+        /// Is this Warehouse used as default.
+        /// </summary>
+        /// <value>Flag</value>
+        [JsonProperty("is_default", Required = Required.Always)]
+        public bool? IsDefault { get; set; }
+    }
+}
---
+++ Source/BreakingBit.Hitmeister.API/Models/WarehouseAddress.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+
+namespace BreakingBit.Hitmeister.API.Models
+{
+    /// <summary>
+    /// The particulars of the place where a warehouse is situated.
+    /// </summary>
+    public sealed class WarehouseAddress
+    {
+        /// <summary>
+        /// Country code according to ISO 3166-2
+        /// </summary>
+        /// <value>Country/Region code</value>
+        [JsonProperty("country", Required = Required.Always)]
+        public Location Country { get; set; }
+
+        /// <summary>
+        /// The name of the street unique in its city
+        /// </summary>
+        /// <value>User written string</value>
+        [JsonProperty("street", Required = Required.Always)]
+        public string Street { get; set; }
+
+        /// <summary>
+        /// The house number may have arbitrary formats
+        /// </summary>
+        /// <value>A user written house number</value>
+        [JsonProperty("house_number", Required = Required.Always)]
+        public string HouseNumber { get; set; }
+
+        /// <summary>
+        /// An identifier for the city unique in its country
+        /// </summary>
+        /// <value>A composition of digits, may have leading zeros</value>
+        [JsonProperty("postcode", Required = Required.Always)]
+        public string Postcode { get; set; }
+
+        /// <summary>
+        /// The name of the city
+        /// </summary>
+        /// <value>User written string</value>
+        [JsonProperty("city", Required = Required.Always)]
+        public string City { get; set; }
+
+        /// <summary>
+        /// The phone number for calling
+        /// </summary>
+        /// <value>An arbitrary phone number format</value>
+        [JsonProperty("phone", Required = Required.Always)]
+        public string PhoneNumber { get; set; }
+    }
+}

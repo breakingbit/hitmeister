681ab064832f6bdac1f80fe4e90d1dcf1b00f46d
michael-herwig <michael.herwig@hotmail.de>
2018-02-05 21:07:31 +0100

    Added ShippingGroup model

---
+++ Source/BreakingBit.Hitmeister.API/Models/ShippingGroup.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace BreakingBit.Hitmeister.API.Models
+{
+    /// <summary>
+    /// A shipping definition created with the Hitmeister back end.
+    /// </summary>
+    public sealed class ShippingGroup
+    {
+        /// <summary>
+        /// A unique name for this ShippingGroup given by the user.
+        /// </summary>
+        /// <value>Unique user defined identifier</value>
+        [JsonProperty("name", Required = Required.Always)]
+        public string Name { get; set; }
+    }
+}

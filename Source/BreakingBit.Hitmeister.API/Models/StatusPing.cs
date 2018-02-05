9031455ffe97e8f532dc56ffcb8d2cea664d2c90
michael-herwig <michael.herwig@hotmail.de>
2018-02-05 13:05:18 +0100

    Added ping model

---
+++ Source/BreakingBit.Hitmeister.API/Models/StatusPing.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace BreakingBit.Hitmeister.API.Models
+{
+    /// <summary>
+    /// Result of a status ping to the Hitmeister API
+    /// </summary>
+    public sealed class StatusPing
+    {
+        /// <summary>
+        /// Response message of an API status ping
+        /// </summary>
+        /// <value>
+        /// "Never tell me the odds." if successful
+        /// </value>
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}

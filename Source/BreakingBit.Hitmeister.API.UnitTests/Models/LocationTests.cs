99263c1c087ef021602a0396b3db80760072a7c9
michael-herwig <michael.herwig@hotmail.de>
2018-02-05 22:26:46 +0100

    Improved tests for Enums with equally values

--- Source/BreakingBit.Hitmeister.API.UnitTests/Models/LocationTests.cs
+++ Source/BreakingBit.Hitmeister.API.UnitTests/Models/LocationTests.cs
@@ -1,4 +1,6 @@
-﻿using BreakingBit.Hitmeister.API.Models;
+﻿using System;
+using System.Linq;
+using BreakingBit.Hitmeister.API.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BreakingBit.Hitmeister.API.UnitTests.Models
@@ -10,6 +12,24 @@ namespace BreakingBit.Hitmeister.API.UnitTests.Models
         public void UnitedKingdomAndGreatBritianTreatedTheSame()
         {
             Assert.AreEqual(Location.UnitedKingdom, Location.GreatBritian);
+        }
+
+        [TestMethod]
+        public void NoOtherThanUnitedKingdomAndGreatBritianAreTreatedTheSame()
+        {
+            foreach (var item1 in Enum.GetValues(typeof(Location)).Cast<Location>())
+            {
+                if (item1 == Location.UnitedKingdom)
+                    continue;
+                foreach (var item2 in Enum.GetValues(typeof(Location)).Cast<Location>())
+                {
+                    if (item2 == Location.UnitedKingdom)
+                        continue;
+                    if (item1.ToString() == item2.ToString())
+                        continue;
+                    Assert.AreNotEqual(item1, item2);
+                }
+            }
         }
     }
 }

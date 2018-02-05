3359369c19f5f185c2b825e12248e3e96c47e354
michael-herwig <michael.herwig@hotmail.de>
2018-02-05 21:30:00 +0100

    Added models for Subscription

---
+++ Source/BreakingBit.Hitmeister.API/Models/Subscription.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+
+namespace BreakingBit.Hitmeister.API.Models
+{
+    /// <summary>
+    /// A configuration to retrieve push notifications from the
+    /// Hitmeister system.
+    /// </summary>
+    public sealed class Subscription
+    {
+        /// <summary>
+        /// The name of the event subscribing to, see SubscriptionEvent.
+        /// </summary>
+        [JsonProperty("event_name", Required = Required.Always)]
+        public SubscriptionEvent Event { get; set; }
+
+        /// <summary>
+        /// Internal identifier of Subscription, unique across all Subscriptions
+        /// </summary>
+        /// <value>Unique identifier</value>
+        [JsonProperty("id_subscription", Required = Required.Always)]
+        public long Identifier { get; set; }
+
+        /// <summary>
+        /// A publicly-accessible URL that will receive a POST request for each event notification.
+        /// </summary>
+        /// <value>Callback URL</value>
+        [JsonProperty("callback_url", Required = Required.Always)]
+        public string CallbackUrl { get; set; }
+
+        /// <summary>
+        /// An email address that will receive an email if the Hitmeister system can’t reach the callback 
+        /// URL for an extended period of time and the subscription gets disabled.
+        /// </summary>
+        /// <value>Email address</value>
+        [JsonProperty("fallback_email", Required = Required.Always)]
+        public string FallbackEmail { get; set; }
+
+        /// <summary>
+        /// If IsActive is set to true the Hitmeister system will try to POST notification to this 
+        /// Subscription otherwise it will be ignored.
+        /// </summary>
+        /// <value>Active/Inactive flag</value>
+        [JsonProperty("is_active", Required = Required.Always)]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Source/BreakingBit.Hitmeister.API/Models/SubscriptionEvent.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace BreakingBit.Hitmeister.API.Models
+{
+    /// <summary>
+    /// Event name
+    /// </summary>
+    /// <value>Event name</value>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum SubscriptionEvent
+    {
+
+        /// <summary>
+        /// This event is triggered once each time a customer places an order 
+        /// which includes one or more of your items.
+        /// </summary>
+        /// <remarks>
+        /// If a customer buys 5 items from you in a single order, only 1 OrderNew 
+        /// event notification will be sent.
+        /// </remarks>
+        [EnumMember(Value = "order_new")]
+        OrderNew,
+
+        /// <summary>
+        /// This event is triggered each time one of your units is ordered.
+        /// </summary>
+        /// <remarks>
+        /// If a customer buys 5 items from you in a single order, 5 separate 
+        /// OrderUnitNew notifications will be sent.
+        /// </remarks>
+        [EnumMember(Value = "order_unit_new")]
+        OrderUnitNew,
+
+        /// <summary>
+        ///  This event is triggered each time when the status of one of your units 
+        ///  has been changed.
+        /// </summary>
+        [EnumMember(Value = "order_unit_status_changed")]
+        OrderUnitStatusChanged,
+
+        /// <summary>
+        /// This event is triggered each time when an item has changed.
+        /// </summary>
+        [EnumMember(Value = "item_changed")]
+        ItemChanged,
+
+        /// <summary>
+        /// This event is triggered each time a category has changed.
+        /// </summary>
+        [EnumMember(Value = "category_changed")]
+        CategoryChanged,
+
+        /// <summary>
+        /// This event is triggered each time a customer starts a return process.
+        /// </summary>
+        [EnumMember(Value = "return_new")]
+        ReturnNew
+    }
+}
